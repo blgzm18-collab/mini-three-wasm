d42fde3f4bf892428247dcee735c4084b3f0b49c
blgzm18-collab <blgzm18@gmail.com>
2026-08-03 22:41:32 -0400

    New files

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,1 +1,10 @@
+#include <emscripten/bind.h>
+#include "core/renderer.h"
 
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(mini_three) {
+    class_<Renderer>("Renderer")
+        .constructor<int,int>()
+        .function("clear", &Renderer::clear);
+}
---
+++ src/core/renderer.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Renderer {
+public:
+    Renderer(int width, int height);
+    void clear(float r, float g, float b);
+};
